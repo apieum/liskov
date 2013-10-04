4665282ec5f146e0e6f0c3209ecf1c744d10031e
Grégory Salvan <apieum@gmail.com>
2013-10-04 09:13:58 +0200

    add fixtures path to sys path and remove fixtures prefix from module import

--- liskov/testLiskov.py
+++ liskov/testLiskov.py
@@ -1,33 +1,34 @@
 # -*- coding: utf8 -*-
 import unittest
-from liskov import subtypeof, behave_as, can_substitute
-
+from liskov import *
+import os.path
+__dir__ = os.path.dirname(os.path.abspath(__file__))
+append_sys_path(os.path.join(__dir__, 'fixtures'))
 
 class LiskovSubstitutionTest(unittest.TestCase):
 
     def test_can_substitute_decorator_create_new_subtype(self):
-        @can_substitute('fixtures.calc.BasicCalc', 'fixtures.convert.BasesConvert')
+        @can_substitute('calc.BasicCalc', 'convert.BasesConvert')
         class ScientificCalc(object):
             pass
 
-        from fixtures.calc import BasicCalc
-        from fixtures.convert import BasesConvert
+        from calc import BasicCalc
+        from convert import BasesConvert
         assert issubclass(ScientificCalc, (BasicCalc, BasesConvert))
 
 
     def test_substitute_metaclass_create_new_subtype(self):
         class ScientificCalc(object):
-            __metaclass__ = behave_as('fixtures.calc.BasicCalc', 'fixtures.convert.BasesConvert')
+            __metaclass__ = behave_as('calc.BasicCalc', 'convert.BasesConvert')
 
-        from fixtures.calc import BasicCalc
-        from fixtures.convert import BasesConvert
+        from calc import BasicCalc
+        from convert import BasesConvert
         assert issubclass(ScientificCalc, (BasicCalc, BasesConvert))
 
 
     def test_subtypeof_import_a_class_in_nested_scope(self):
-        class ScientificCalc(subtypeof('fixtures.calc.BasicCalc')):
+        class ScientificCalc(subtypeof('calc.BasicCalc')):
             pass
 
-        from fixtures.calc import BasicCalc
+        from calc import BasicCalc
         assert issubclass(ScientificCalc, BasicCalc)
-
