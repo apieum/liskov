711c1ba928d2570fe16a8eaf2dc986da83fd273f
Grégory Salvan <apieum@gmail.com>
2013-10-04 12:50:52 +0200

    added constraints for subtype + refactoring: change names and move functions

--- liskov/liskov.py
+++ liskov/liskov.py
@@ -1,48 +1,35 @@
 # -*- coding: utf8 -*-
 
-__all__=['behave_as', 'can_substitute', 'subtypeof', 'append_sys_path', 'apart_from']
+__all__=['behave_as', 'can_substitute', 'subtype', 'append_sys_path', 'constrain', 'under_constraint']
 
 
 def behave_as(*bases):
-    substitute_bases = __load_bases(bases)
+    substitute_bases = _load_bases(bases)
 
     class Liskov(type):
         constraints = tuple()
         def __new__(cls, name, bases, attrs):
-            new_bases = _remove_behaviours(substitute_bases, cls.constraints)
+            new_bases = _remove_bases_behaviours(substitute_bases, cls.constraints)
             return type.__new__(cls, name, new_bases + bases, attrs)
 
         @classmethod
-        def apart_from(cls, *behaviours):
+        def except_for(cls, *behaviours):
             cls.constraints = behaviours
             return cls
 
     return Liskov
 
-def subtypeof(base):
+def subtype(base):
     mod_name, sep, cls_name = base.rpartition('.')
     module = __import__(mod_name, globals(), locals(), [cls_name])
     return getattr(module, cls_name)
 
-def _remove_behaviours(bases, behaviours):
-    new_bases = tuple()
-    for base in bases:
-        base_attrs = base.__dict__
-        attrs = dict([
-            (key, value) for key, value in base_attrs.items()
-            if key not in behaviours
-        ])
-        if len(base_attrs) > len(attrs):
-            base = type(base.__name__, base.__bases__, attrs)
-        new_bases += (base, )
-    return new_bases
-
 def can_substitute(*bases):
-    substitute_bases = __load_bases(bases)
+    substitute_bases = _load_bases(bases)
 
     def liskov(cls, substitute_bases=substitute_bases):
         if hasattr(cls, 'constrained_subtype'):
-            substitute_bases = _remove_behaviours(substitute_bases, cls.constrained_subtype)
+            substitute_bases = _remove_bases_behaviours(substitute_bases, cls.constrained_subtype)
 
         bases = (cls, ) + substitute_bases + cls.__bases__
         attrs = dict(cls.__dict__)
@@ -51,6 +38,23 @@ def can_substitute(*bases):
 
     return liskov
 
+def under_constraint(*behaviours):
+    def constraints(cls):
+        setattr(cls, 'constrained_subtype', behaviours)
+        return cls
+    return constraints
+
+class constrain(object):
+    def __init__(self, *behaviours):
+        self.constraints = behaviours
+
+    def __rand__(self, cls):
+        bases = _remove_bases_behaviours(cls.__bases__, self.constraints)
+        attrs = _remove_behaviours(cls.__dict__, self.constraints)
+        return type(cls.__name__, bases, attrs)
+
+    __ror__ = __radd__ = __rsub__ = __rand__
+
 def append_sys_path(path):
     import sys, os
     if os.path.isfile(path):
@@ -58,16 +62,28 @@ def append_sys_path(path):
 
     sys.path.append(os.path.realpath(path))
 
-def __load_bases(bases):
+def _remove_bases_behaviours(bases, behaviours):
+    new_bases = tuple()
+    for base in bases:
+        base_attrs = base.__dict__
+        attrs = _remove_behaviours(base_attrs, behaviours)
+        if len(base_attrs) > len(attrs):
+            base = type(base.__name__, base.__bases__, attrs)
+        new_bases += (base, )
+    return new_bases
+
+def _remove_behaviours(attrs, behaviours):
+    new_attrs = dict()
+    for key, value in attrs.items():
+        if key not in behaviours:
+            new_attrs[key] = value
+    return new_attrs
+
+
+def _load_bases(bases):
     substitute_bases = tuple()
     for base in bases:
-        cls = subtypeof(base)
+        cls = subtype(base)
         if cls not in substitute_bases:
             substitute_bases+= (cls, )
     return substitute_bases
-
-def apart_from(*behaviours):
-    def constraints(cls):
-        setattr(cls, 'constrained_subtype', behaviours)
-        return cls
-    return constraints
--- liskov/testLiskov.py
+++ liskov/testLiskov.py
@@ -1,9 +1,13 @@
 # -*- coding: utf8 -*-
 import unittest
 from liskov import *
+
 import os.path
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 append_sys_path(os.path.join(__dir__, 'fixtures'))
+
+import elephant
+
 
 class LiskovSubstitutionTest(unittest.TestCase):
 
@@ -27,17 +31,16 @@ class LiskovSubstitutionTest(unittest.TestCase):
 
 
     def test_subtypeof_import_a_class_in_nested_scope(self):
-        class ScientificCalc(subtypeof('calc.BasicCalc')):
+        class ScientificCalc(subtype('calc.BasicCalc')):
             pass
 
         from calc import BasicCalc
         assert issubclass(ScientificCalc, BasicCalc)
 
-import elephant
 class ConstrainedSubtypeTest(unittest.TestCase):
     def test_can_substitute_except_constraints(self):
         @can_substitute('elephant.ElephantTest')
-        @apart_from('test_it_can_be_grey', 'test_it_can_be_white')
+        @under_constraint('test_it_can_be_grey', 'test_it_can_be_white')
         class RoyalElephantTest(object):
             def new_elephant(self, color):
                 return elephant.RoyalElephant(color)
@@ -46,7 +49,14 @@ class ConstrainedSubtypeTest(unittest.TestCase):
 
     def test_behave_as_except_constraints(self):
         class RoyalElephantTest(object):
-            __metaclass__ = behave_as('elephant.ElephantTest').apart_from('test_it_can_be_grey', 'test_it_can_be_white')
+            __metaclass__ = behave_as('elephant.ElephantTest').except_for('test_it_can_be_grey', 'test_it_can_be_white')
+            def new_elephant(self, color):
+                return elephant.RoyalElephant(color)
+
+        self.__run(RoyalElephantTest)
+
+    def test_subtypeof_except_constraints(self):
+        class RoyalElephantTest(subtype('elephant.ElephantTest') & constrain('test_it_can_be_grey', 'test_it_can_be_white')):
             def new_elephant(self, color):
                 return elephant.RoyalElephant(color)
 
